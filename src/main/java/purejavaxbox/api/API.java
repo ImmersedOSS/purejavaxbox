57f263727945b0f22085f51d45e339fed11122c2
Daniel Avila <masterav10@gmail.com>
2017-01-28 12:18:11 -0500

    Expanding the surface of ControllerAPI.

---
+++ src/main/java/purejavaxbox/api/API.java
@@ -0,0 +1,35 @@
+package purejavaxbox.api;
+
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
+
+/**
+ * Utility class to prevent duplication and improve readability of {@link ControllerApi}.
+ */
+final class API
+{
+    /**
+     * The predicate created by this function is used to filter out values in the observeAfterDelay(..) functions.
+     *
+     * @param count     - the number of ticks since the button was first held down.
+     * @param timestamp - the timestamp indicating when the buttons(s) were first pressed down.
+     * @param timeNanos - the measure of the delay in nanos.
+     * @param <T>       - the type of value to consume. Value is unused in the calculation.
+     * @return returns true if count == 0, or the button(s) have been held longer than timeNanos.
+     */
+    static <T> Predicate<T> duringDelay(AtomicLong count, AtomicLong timestamp, long timeNanos)
+    {
+        return v -> {
+            boolean isFirst = count.getAndIncrement() == 0;
+            long elapsed = System.nanoTime() - timestamp.get();
+            long diff = elapsed - timeNanos;
+            boolean timeHasExceededDelay = diff >= 0;
+            return isFirst || timeHasExceededDelay;
+        };
+    }
+
+    private API()
+    {
+        // Utility class
+    }
+}
--- src/main/java/purejavaxbox/api/ControllerApi.java
+++ src/main/java/purejavaxbox/api/ControllerApi.java
@@ -4,8 +4,12 @@ import purejavaxbox.XboxButton;
 import reactor.core.publisher.Flux;
 
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Function;
 import java.util.function.Supplier;
+
+import static purejavaxbox.api.API.*;
 
 /**
  * This interface provides the entry point for developers to listen to a controller. Default methods in this class
@@ -31,12 +35,13 @@ public interface ControllerApi extends Supplier<Flux<Map<XboxButton, Number>>>
      * Watch a particular button. This method will update as quickly as polling.
      *
      * @param button - the button to observe.
-     * @return a flux holding the value.
+     * @return a flux holding the value of the button.
      */
     default Flux<Number> observe(XboxButton button)
     {
-        return get().filter(m -> !m.isEmpty())
-                    .map(m -> m.get(button));
+        return get()
+                .filter(m -> !m.isEmpty())
+                .map(m -> m.get(button));
     }
 
     /**
@@ -48,22 +53,39 @@ public interface ControllerApi extends Supplier<Flux<Map<XboxButton, Number>>>
      * @return a flux which will broadcast true a single time when all buttons are pressed. Once the condition is no
      * longer true, a false is sent.
      */
-    default Flux<Boolean> observeCombo(XboxButton b1, XboxButton b2, XboxButton... buttons)
+    default Flux<Boolean> observeToggle(XboxButton b1, XboxButton b2, XboxButton... buttons)
+    {
+        return observe(b1, b2, buttons).distinctUntilChanged();
+    }
+
+    /**
+     * Observe the state of a button combination. The returned flux will produce true only when all provided buttons are
+     * pressed.
+     *
+     * @param b1      - the  first button in the combination.
+     * @param b2      - the second button in the combination.
+     * @param buttons - any additional buttons.
+     * @return a flux which will broadcast true when all buttons are pressed during each polling event, otherwise false
+     * will be broadcast.
+     */
+    default Flux<Boolean> observe(XboxButton b1, XboxButton b2, XboxButton... buttons)
     {
         Function<? super Number, ? extends Boolean> mapper = n -> n.intValue() == 1;
 
         Flux<Boolean> flux = observe(b1).map(mapper);
 
-        flux = flux.zipWith(observe(b2).map(mapper))
-                   .map(t -> t.getT1() && t.getT2());
+        flux = flux
+                .zipWith(observe(b2).map(mapper))
+                .map(t -> t.getT1() && t.getT2());
 
         for (XboxButton button : buttons)
         {
-            flux = flux.zipWith(observe(button).map(mapper))
-                       .map(t -> t.getT1() && t.getT2());
+            flux = flux
+                    .zipWith(observe(button).map(mapper))
+                    .map(t -> t.getT1() && t.getT2());
         }
 
-        return flux.distinctUntilChanged();
+        return flux;
     }
 
     /**
@@ -74,7 +96,120 @@ public interface ControllerApi extends Supplier<Flux<Map<XboxButton, Number>>>
      */
     default Flux<Boolean> observeToggle(XboxButton button)
     {
-        return observe(button).distinctUntilChanged()
-                              .map(n -> n.intValue() == 1);
+        return observe(button)
+                .distinctUntilChanged()
+                .map(n -> n.intValue() == 1);
+    }
+
+    /**
+     * Observe when a button has been held down for the specified duration.
+     *
+     * @param duration - measure of time.
+     * @param unit     - unit of duration.
+     * @param button   - the first button.
+     * @return a flux which will broadcast true when the combination of buttons have been held for the specified
+     * duration. False is initially broadcast, or if the button is released after this stream has broadcast true.
+     */
+    default Flux<Boolean> observeHeld(long duration, TimeUnit unit, XboxButton button)
+    {
+        long timeToHold = unit.toNanos(duration);
+
+        AtomicLong timestamp = new AtomicLong();
+        return observe(button)
+                .map(n -> n.intValue() == 1)
+                .doOnNext(pressed -> {
+                    if (!pressed)
+                    {
+                        timestamp.set(System.nanoTime());
+                    }
+                })
+                .filter(pressed -> System.nanoTime() - timestamp.get() > timeToHold || !pressed)
+                .distinctUntilChanged();
+    }
+
+    /**
+     * Observe when a combination of buttons has been held down for the specified duration.
+     *
+     * @param duration - measure of time.
+     * @param unit     - unit of duration.
+     * @param b1       - the first button.
+     * @param b2       - the second button.
+     * @param buttons  - for three or more buttons.
+     * @return a flux which will broadcast true when the combination of buttons have been held for the specified
+     * duration. False is initially broadcast, or if one of the buttons is released after this stream has broadcast
+     * true.
+     */
+    default Flux<Boolean> observeHeld(long duration, TimeUnit unit, XboxButton b1, XboxButton b2, XboxButton... buttons)
+    {
+        Flux<Boolean> observeAll = observe(b1, b2, buttons);
+
+        long timeToHold = unit.toNanos(duration);
+        AtomicLong timestamp = new AtomicLong();
+        return observeAll
+                .doOnNext(pressed -> {
+                    if (!pressed)
+                    {
+                        timestamp.set(System.nanoTime());
+                    }
+                })
+                .filter(pressed -> System.nanoTime() - timestamp.get() > timeToHold || !pressed)
+                .distinctUntilChanged();
+    }
+
+    /**
+     * Notifies listeners when a value is pressed, and then provides a stream of values after the specified duration has
+     * elapsed and the button is still being held.
+     *
+     * @param duration - the amount of time to delay before streaming values.
+     * @param unit     - the measure of duration.
+     * @return a flux holding the value of the button.
+     * @apram button - the button to watch.
+     */
+    default Flux<Number> observeAfterDelay(long duration, TimeUnit unit, XboxButton button)
+    {
+        AtomicLong count = new AtomicLong(0);
+        AtomicLong timestamp = new AtomicLong(0);
+
+        long time = unit.toNanos(duration);
+
+        return observe(button)
+                .filter(n -> {
+                    boolean isPressed = n.intValue() == 0;
+                    if (!isPressed)
+                    {
+                        count.set(0);
+                        timestamp.set(System.nanoTime());
+                    }
+                    return isPressed;
+                })
+                .filter(duringDelay(count, timestamp, time));
+    }
+
+    /**
+     * Notifies listeners when a value is pressed, and then provides a stream of values after the specified duration has
+     * elapsed and the button is still being held.
+     *
+     * @param duration - the amount of time to delay before streaming values.
+     * @param unit     - the measure of duration.
+     * @return a flux holding the value of the button.
+     * @apram button - the button to watch.
+     */
+    default Flux<Boolean> observeAfterDelay(long duration, TimeUnit unit, XboxButton b1, XboxButton b2, XboxButton... buttons)
+    {
+        AtomicLong count = new AtomicLong(0);
+        AtomicLong timestamp = new AtomicLong(0);
+
+        long time = unit.toNanos(duration);
+
+        return observe(b1, b2, buttons)
+                .filter(isPressed -> {
+                    if (!isPressed)
+                    {
+                        count.set(0);
+                        timestamp.set(System.nanoTime());
+                    }
+                    return isPressed;
+                })
+                .filter(duringDelay(count, timestamp, time));
     }
 }
