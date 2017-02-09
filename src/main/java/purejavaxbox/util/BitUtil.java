8f9f6f204208335c33d7215473c47f28533293e9
Daniel Avila <masterav10@gmail.com>
2017-02-09 02:14:07 -0500

    Removed unused method.

--- src/main/java/purejavaxbox/util/BitUtil.java
+++ src/main/java/purejavaxbox/util/BitUtil.java
@@ -8,18 +8,6 @@ public final class BitUtil
     }
 
     /**
-     * Extracts the value of a bit in a byte.
-     *
-     * @param target - the short to extract the bit from.
-     * @param index  - the index. Must between 0 and 7. Behavior for values outside of this range is undefined.
-     * @return 0 if the bit is not set, 1 if it is.
-     */
-    public static int getBitFrom(byte target, int index)
-    {
-        return getBitFrom(Byte.toUnsignedInt(target), index);
-    }
-
-    /**
      * Extracts the value of a bit in a short.
      *
      * @param target - the short to extract the bit from.
